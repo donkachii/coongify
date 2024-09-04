5d18d6114847d18f8d55a6a3dd0b0f867a3a4b0f
Ijeoma Igboagu <ijeonyi@gmail.com>
2024-09-04 10:20:16 +0100

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-import Header from "@/components/navbar";
-import Image from "next/image";
+import Navbar from "../components/Navbar"
 
 export default function Home() {
   return (
     <>
-      <Header />
+      <Navbar/>
     </>
   );
 }
