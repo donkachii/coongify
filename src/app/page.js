c8369a9581f04187444a15cfdf546364bebe8bc9
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2024-10-01 06:39:21 +0100

    feat: Refactor Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,16 @@
-import Navbar from "../components/Navbar"
-import Heros from '../components/Hero';
-import Features from '../components/Features';
-import About from '../components/About';
-import Footer from '../components/Footer';
+import Navbar from "../components/Navbar";
+import Heros from "../components/Hero";
+import Features from "../components/Features";
+import About from "../components/About";
+import Footer from "../components/Footer";
 export default function Home() {
-  return(
+  return (
     <>
       <Navbar />
       <Heros />
       <Features />
       <About />
-      <Footer/>
+      <Footer />
     </>
   );
 }
